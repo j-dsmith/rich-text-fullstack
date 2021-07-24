ba5351c94afcdb3295f1a0b7480371d41eef13ec
Jamey Smith <smith.jameyd@gmail.com>
2021-07-24 10:04:02 -0700

    Static file structure fix in server

--- server/index.js
+++ server/index.js
@@ -31,7 +31,7 @@ require("./routes/authRoutes")(app);
 require("./routes/projectRoutes")(app);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+  app.use(express.static(path.join(__dirname, "client/build")));
 
   const path = require("path");
   app.get("*", (req, res) => {
