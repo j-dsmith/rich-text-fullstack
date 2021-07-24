351b71142feda7fbd3726474210b65b2c15ad87a
Jamey Smith <smith.jameyd@gmail.com>
2021-07-24 09:57:30 -0700

    Postbuild script fixes

--- server/index.js
+++ server/index.js
@@ -3,7 +3,6 @@ const cookieSession = require("cookie-session");
 const passport = require("passport");
 const mongoose = require("mongoose");
 const keys = require("./config/keys");
-const path = require("path");
 require("./models/User");
 require("./models/Project");
 require("./services/passport");
@@ -15,7 +14,6 @@ mongoose.connect(keys.mongoURI, {
 });
 
 const app = express();
-app.use("/static", express.static(path.join(__dirname, "client/build")));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
@@ -32,10 +30,14 @@ app.use(passport.session());
 require("./routes/authRoutes")(app);
 require("./routes/projectRoutes")(app);
 
-// app.use((err, req, res, next) => {
-//   //console.log(err)
-//   res.status(422).send({ error: error.message });
-// });
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+
+  const path = require("path");
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve((__dirname, "client", "build", "index.html")));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
