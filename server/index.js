3ade03b83f459cad8d017af4d522ed67163092de
Jamey Smith <smith.jameyd@gmail.com>
2021-07-24 10:06:57 -0700

    Express.static refactor

--- server/index.js
+++ server/index.js
@@ -31,9 +31,9 @@ require("./routes/authRoutes")(app);
 require("./routes/projectRoutes")(app);
 
 if (process.env.NODE_ENV === "production") {
+  const path = require("path");
   app.use(express.static(path.join(__dirname, "client/build")));
 
-  const path = require("path");
   app.get("*", (req, res) => {
     res.sendFile(path.resolve((__dirname, "client", "build", "index.html")));
   });
